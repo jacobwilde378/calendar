24248150c94582d96061f2c7b9ef88ac2365c1f8
Jacob.Wilde <jacob.wilde@utah.edu>
2020-08-30 20:43:08 -0600

    my bad

--- Develop/mystuff.js
+++ Develop/mystuff.js
@@ -1,14 +1,21 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
 //var hour = moment().format("H")
 var hour = 12
-console.log(hour)
 var timeSlots = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+localStorage.setItem('task-desc-10',"AAA")
+localStorage.setItem('task-desc-12',"bbb")
+localStorage.setItem('task-desc-14',"ccc")
+
+
+
+
 for (i = 0; i < timeSlots.length; i++) {
     if (timeSlots[i] === hour) {
-        classes = "row time-block present"
+        classes = "row  time-block present"
     }
     else if (timeSlots[i] < hour) {
-        classes = "row time-block past"
+        classes = "row  time-block past"
     }
     else {
         classes = "row time-block future"
@@ -17,12 +24,37 @@ for (i = 0; i < timeSlots.length; i++) {
     var createRow = $("<div>")
         .attr("id", "hour-" + timeSlots[i])
         .attr("class", classes)
-        
-    
+
+    var timeCol = $("<div>")
+        .attr("class", "col-1 hour")
+        .text(timeSlots[i])
+
+
+    var textCol = $("<div>")
+        .attr("class", "col-10 description")
+
+    var textArea = $("<textarea>")
+        .attr("class", "col-12")
+        .text(localStorage.getItem("task-desc-" + timeSlots[i]))
+
+    textCol.append(textArea)
+
+
+    var subButton = $("<button>")
+        .attr("class", "col-1 saveBtn")
+        .attr("id", "subButton-" + timeSlots[i])
+
+    createRow.append(timeCol)
+    createRow.append(textCol)
+    createRow.append(subButton)
+
 
     $(".container").append(createRow)
 
 
-   
+
 }
 //create event listener using 'this'
+$("#master").on('click', function() {
+    console.log(this)
+})
